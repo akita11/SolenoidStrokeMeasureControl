58037a7243e3bc0d5c5209e66c30ef33ee6c59cb
akita11 <akita@ifdl.jp>
2024-05-08 18:47:22 +0900

    add Arduino measure code

--- Measure_ArduinoUNO/src/main.cpp
+++ Measure_ArduinoUNO/src/main.cpp
@@ -2,50 +2,92 @@
 
 #define PIN_FLAG1 2
 #define PIN_FLAG2 3
-#define PIN_ADC A0
+#define PIN_ADC   A0
+#define PIN_PWM   9
+#define PIN_SW    4
 
-uint16_t v0, v1;
+uint32_t v0, v1;
+uint8_t n = 0;
+uint8_t N = 128;
+
+uint16_t Ton = 1000;
+uint16_t Delay = 50;
 
 void setup()
 {
+	pinMode(PIN_PWM, OUTPUT);
 	pinMode(PIN_FLAG1, OUTPUT); digitalWrite(PIN_FLAG1, LOW);
 	pinMode(PIN_FLAG2, OUTPUT); digitalWrite(PIN_FLAG2, LOW);
+	pinMode(PIN_SW, INPUT_PULLUP);
 	Serial.begin(115200);
-	// Timer1 設定
-	TCCR1A = _BV(COM1A1) | _BV(WGM11);			  // 非反転モードでチャネルAを設定し、WGM11=1 (モード14, Fast PWM)
-	TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11); // WGM13:12=11, CS11=1 (プリスケーラ8 = 2MHz)
+	// setup Timer1
+	TCCR1A = _BV(COM1A1) | _BV(WGM11);			  // Ch.A: non-inverting, WGM11=1 (mode14, Fast PWM)
+	TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11); // WGM13:12=11, CS11=1 (16MHz/8 = 2MHz)
 
-	// ICR1 は PWM のトップ値を設定
-	ICR1 = 19999; // 2MHz/20000=1000Hz(10ms)
+	// ICR1 = TOP
+	ICR1 = 19999; // 2MHz/20000=100Hz(10ms)
 
-	OCR1A = 1999; // 2MHz/2000=1kHz / 1ms / PWM Duty Cycle
+//	OCR1A = 3999; // 2MHz/4000=0.5kHz / 2ms / PWM Duty Cycle
+	OCR1A = Ton * 2 - 1; // PWM Duty Cycle
 	OCR1B = 999; // 2MHz/1000=2kHz / 0.5ms
 
-	// 割り込み設定
-	TIMSK1 = _BV(TOIE1);  // enable Timer1 OVF interrupt
-	TIMSK1 = _BV(OCIE1B); // enable Timer1 COMPB interrupt 
-
-	sei(); // グローバル割り込みを有効化
+	// enable interrupts
+	TIMSK1 |= _BV(TOIE1);  // enable Timer1 OVF interrupt
+	TIMSK1 |= _BV(OCIE1B); // enable Timer1 COMPB interrupt 
+	sei(); // enable global interrupt
 }
 // note: TCNT=OCR1AのときにTimer1_COMPA割り込み
 
 // Timer1 のオーバーフロー割り込み
 ISR(TIMER1_OVF_vect)
 {
-	digitalWrite(PIN_FLAG1, 1);
-	digitalWrite(PIN_FLAG1, 0);
-	v0 = analogRead(PIN_ADC);
+	delayMicroseconds(Delay);
+	PORTD |= _BV(PD2);
+	v0 += analogRead(PIN_ADC);
+	PORTD &= ~(_BV(PD2));
 }
 
 // Timer1 のCompareMatchB割り込み
 ISR(TIMER1_COMPB_vect)
 {
-	digitalWrite(PIN_FLAG2, 1);
-	digitalWrite(PIN_FLAG2, 0);
-	v1 = analogRead(PIN_ADC);
-	Serial.print(v0); Serial.print(","); Serial.println(v1);
+	PORTD |= _BV(PD3);
+	v1 += analogRead(PIN_ADC);
+	PORTD &= ~(_BV(PD3));
+	n++;
+	if (n == N) {
+		n = 0;
+		v0 /= N;
+		v1 /= N;
+		Serial.print(v0); Serial.print(","); Serial.println(v1);
+	}
 }
 
+uint8_t st = 0;
 void loop()
 {
+	if (digitalRead(PIN_SW) == LOW) {
+		switch(st){
+			case 0 : Delay = 50; Ton = 1000; break;
+			case 2 : Delay = 50; Ton = 2000; break;
+			case 4 : Delay = 50; Ton = 3000; break;
+			case 6 : Delay = 50; Ton = 4000; break;
+			case 8 : Delay = 50; Ton = 5000; break;
+			case 10 : Delay = 50; Ton = 6000; break;
+			case 12 : Delay = 50; Ton = 7000; break;
+			case 14 : Delay = 50; Ton = 8000; break;
+			case 16 : Delay = 50; Ton = 9000; break;
+			case 1 : Delay = 100; Ton = 1000; break;
+			case 3 : Delay = 100; Ton = 2000; break;
+			case 5 : Delay = 100; Ton = 3000; break;
+			case 7 : Delay = 100; Ton = 4000; break;
+			case 9 : Delay = 100; Ton = 5000; break;
+			case 11 : Delay = 100; Ton = 6000; break;
+			case 13 : Delay = 100; Ton = 7000; break;
+			case 15 : Delay = 100; Ton = 8000; break;
+			case 17 : Delay = 100; Ton = 9000; break;
+		}
+		OCR1A = Ton * 2 - 1; // PWM Duty Cycle
+		st = (st + 1) % 18;
+		while(digitalRead(PIN_SW) == LOW) delay(10);
+	}
 }
