443f427fc79fed44e30ff5226573982739c2c0c5
Junichi Akita <akita@ifdl.jp>
2024-06-03 09:29:49 +0900

    GDS for position prediction

--- Control_ArduinoUNO/src/main.cpp
+++ Control_ArduinoUNO/src/main.cpp
@@ -14,61 +14,108 @@ uint16_t v0, v0_, v1;
 uint16_t Ton = 1000;
 uint16_t Delay = 100;
 
-// for CDS0730058
-/*
-#define X 9
-#define Y 6
-uint16_t ADCvalue[X][Y] = {
-	{75, 82, 101, 116, 144}, // for Ton=1, L[0], L[1], ...
-	{73, 81, 101, 116, 143}, // for Ton=2, L[0], L[1], ...
-	{68, 79, 100, 113, 143},
-	{63, 76, 97, 109, 142},
-	{59, 72, 93, 105, 142},
-	{56, 67, 88, 100, 140},
-	{51, 62, 81, 91, 132},
-	{47, 53, 70, 80, 116},
-	{38, 42, 53, 61, 87}
-};
-float L[] = {123.3, 111.5, 94.43, 86.33, 65.3};
-	// for CDS043 (1st)
-    // L[mH] = -11.861 * S[mm] + 114.53
-    //S = (114.53 - Lint) / 11.861;
-*/
+#define X 9 // Ton
+#define Y 9 // pos
+#define Z 5 // I
 
-// for CDS0730140 (Vs=5V)
-/*
-#define X 9
-#define Y 6
-uint16_t ADCvalue[X][Y] = {
-{36, 43, 50, 58, 67, 75}, // ADCValue[0]
-{36, 43, 50, 57, 66, 74},
-{36, 42, 49, 58, 66, 74},
-{35, 43, 49, 57, 66, 74},
-{33, 39, 48, 58, 66, 75},
-{30, 37, 46, 57, 67, 75},
-{29, 33, 41, 52, 64, 75},
-{25, 28, 35, 45, 55, 66},
-{22, 23, 29, 35, 43, 51}
+// data from LongStrokeSolenoid, Rf=20k
+int ADCvalue[X][Y][Z] = {
+  {
+    // I(01) I(03) I(05) I(07) I(-01)
+    { 43, 58, 68, 75, 82 }, // I for Ton=1ms, pos[0]
+    { 42, 58, 68, 76, 83 }, // I for Ton=1ms, pos[1]
+    { 42, 58, 68, 76, 84 }, // I for Ton=1ms, pos[2]
+    { 41, 58, 68, 77, 85 },
+    { 40, 57, 69, 78, 86 },
+    { 38, 56, 69, 80, 90 },
+    { 36, 56, 70, 82, 93 },
+    { 34, 56, 72, 86, 99 },
+    { 32, 56, 74, 90, 104 }
+  }, {
+    { 108, 123, 132, 140, 174 }, // I for Ton=2ms, pos[0]
+    { 107, 121, 131, 138, 173 }, // I for Ton=2ms, pos[1]
+    { 105, 120, 130, 137, 175 }, // I for Ton=2ms, pos[2]
+    { 103, 119, 129, 137, 176 },
+    { 101, 117, 128, 136, 178 },
+    { 96, 113, 125, 135, 184 },
+    { 91, 110, 123, 134, 190 },
+    { 85, 106, 121, 134, 199 },
+    { 80, 102, 119, 134, 207 }
+  }, {
+    { 178, 192, 201, 208, 261 },
+    { 176, 190, 199, 206, 261 },
+    { 173, 188, 196, 204, 262 },
+    { 171, 185, 195, 202, 264 },
+    { 168, 183, 193, 200, 267 },
+    { 161, 177, 188, 197, 274 },
+    { 154, 172, 184, 194, 281 },
+    { 145, 164, 178, 190, 292 },
+    { 137, 158, 174, 188, 302 }
+  }, {
+    { 249, 263, 272, 278, 347 },
+    { 246, 260, 268, 275, 346 },
+    { 244, 258, 266, 273, 346 },
+    { 241, 255, 264, 271, 348 },
+    { 237, 251, 261, 268, 351 },
+    { 229, 244, 255, 263, 358 },
+    { 221, 237, 249, 258, 365 },
+    { 209, 228, 241, 252, 377 },
+    { 200, 220, 234, 247, 387 }
+  }, {
+    { 321, 335, 343, 350, 431 },
+    { 319, 332, 340, 347, 427 },
+    { 316, 330, 338, 344, 427 },
+    { 313, 327, 335, 342, 428 },
+    { 309, 323, 332, 339, 430 },
+    { 301, 316, 325, 333, 436 },
+    { 292, 308, 318, 327, 443 },
+    { 280, 297, 308, 319, 454 },
+    { 268, 287, 300, 312, 464 }
+  }, {
+    { 390, 404, 413, 419, 516 },
+    { 390, 403, 411, 417, 510 },
+    { 389, 402, 410, 416, 507 },
+    { 388, 400, 408, 414, 506 },
+    { 385, 397, 405, 411, 506 },
+    { 377, 390, 399, 405, 511 },
+    { 368, 382, 392, 399, 517 },
+    { 355, 371, 382, 390, 526 },
+    { 343, 361, 372, 382, 535 }
+  }, {
+    { 458, 473, 482, 489, 597 },
+    { 460, 474, 482, 488, 589 },
+    { 461, 474, 482, 488, 585 },
+    { 462, 474, 481, 487, 582 },
+    { 461, 473, 480, 486, 579 },
+    { 456, 468, 476, 482, 581 },
+    { 448, 461, 470, 476, 585 },
+    { 437, 451, 460, 467, 592 },
+    { 425, 441, 451, 459, 599 }
+  }, {
+    { 527, 544, 554, 562, 669 },
+    { 532, 547, 556, 562, 662 },
+    { 536, 550, 558, 564, 656 },
+    { 539, 552, 559, 564, 652 },
+    { 540, 552, 559, 564, 649 },
+    { 540, 551, 558, 563, 647 },
+    { 535, 546, 554, 559, 649 },
+    { 526, 538, 546, 551, 653 },
+    { 516, 530, 538, 545, 657 }
+  }, {
+    { 617, 635, 644, 651, 726 },
+    { 619, 635, 644, 650, 721 },
+    { 622, 636, 644, 650, 718 },
+    { 626, 639, 646, 651, 714 },
+    { 629, 641, 647, 651, 710 },
+    { 632, 642, 648, 652, 708 },
+    { 630, 640, 646, 650, 707 },
+    { 625, 635, 641, 645, 709 },
+    { 619, 630, 636, 640, 710 }
+  }
 };
-float Pos[] = {0.0, 1.0, 2.08, 3.03, 3.96, 4.82};
-*/
+float pos[Y] = {12.24, 14.25, 16.31, 18.2, 20.34, 24.36, 28.12, 32.31, 35.05};
 
-// for CDS0730140 (Vs=6V, Rf=30k)
-#define X 9
-#define Y 6
-uint16_t ADCvalue[X][Y] = {
-{43, 50, 59, 69, 80, 87},
-{42, 50, 59, 68, 80, 88},
-{42, 49, 59, 68, 79, 87},
-{40, 48, 59, 68, 79, 87},
-{38, 46, 58, 68, 80, 87},
-{36, 43, 54, 65, 80, 87},
-{34, 39, 49, 61, 76, 86},
-{30, 34, 43, 53, 67, 77},
-{25, 28, 33, 40, 50, 59}
-};
-float Pos[] = {0, 1, 2.08, 3.03, 3.96, 4.82};
-
+int vt[2];
 
 void setup()
 {
@@ -85,7 +132,9 @@ void setup()
 
 	ICR1 = 19999;		 // 2MHz/20000=100Hz(10ms) / TOP
 	OCR1A = Ton * 2 - 1; // PWM Duty Cycle
-	OCR1B = 999;		 // 2MHz/1000=2kHz / 0.5ms
+	// OCR1B/2MHz*1000 = T[ms] ; OCR1B = T[ms]*2000
+//	OCR1B = 999;		 // 0.5ms
+	OCR1B = 1399;		 // 0.7ms
 
 	// enable interrupts
 	TIMSK1 |= _BV(TOIE1);  // enable Timer1 OVF interrupt (=PWM ON)
@@ -93,7 +142,6 @@ void setup()
 	sei();				   // enable global interrupt
 
 }
-
 
 // Timer1 のオーバーフロー割り込み (=PWM ON)
 ISR(TIMER1_OVF_vect)
@@ -110,87 +158,100 @@ ISR(TIMER1_COMPB_vect)
 //	PORTD |= _BV(PD3);
 	v1 = analogRead(PIN_ADC);
 	v0 = v0_;
+	vt[0] = v0_;
+	vt[1] = v1;
 //	PORTD &= ~(_BV(PD3));
 }
 
 uint16_t tm = 0;
 
-float calc_pos(int Ton, int ADCval)
+#define Dpos (pos[Y-1] - pos[0])
+
+float calcV(float Ton, int i_pos, int i_param)
 {
-	uint8_t x, y;
-	x = 0; while(x < X - 1){
-	  if ((uint16_t)(x+1) * 1000 <= Ton && Ton < (uint16_t)(x+2)*1000) break;
-	  x++;
-	}
-	float x0 = (float)(x + 1);
-	float t = ((float)Ton / 1000.0 - x0);
-	if (t < 0.0) t = 0.0;
-	else if (t > 1.0) t = 1.0;
-	float s;
-    y = 0; while(y < Y - 1){
-		float y01 = (1.0 - t) * (float)ADCvalue[x][y] + t * (float)ADCvalue[x+1][y];
-		float y23 = (1.0 - t) * (float)ADCvalue[x][y+1] + t * (float)ADCvalue[x+1][y+1];
-/*
-		Serial.print(ADCval);
-		Serial.print(' ');
-		Serial.print(y);
-		Serial.print(' ');
-		Serial.print(y01);
-		Serial.print(' ');
-		Serial.println(y23);
-*/
-		s = ((float)ADCval - y01) / (y23 - y01);
-		if (0.0 <= s && s <= 1.0) break;
-		y++;
-	}
-/*
-	// for CDS0730058
-	float Lint;
-    float S;
-	if (ADCval < ADCvalue[x][0]) Lint = L[0];
-	else if (y < Y - 1){
-	  Lint = (1 - s) * L[y] + s * L[y+1];
-	}
-	else{
- 	  Lint = L[Y - 1];
-	}
-	// for CDS0730, 1st
-    // L[mH] = -11.861 * S[mm] + 114.53
-    //S = (114.53 - Lint) / 11.861;
-	// for LongStroke
-	// L[mH] = -0.823 * S[mm] + 118.3
-    S = (118.3 - Lint) / 0.823;
-*/
-	float Pos_int;
-	if (ADCval < ADCvalue[x][0]) Pos_int = Pos[0];
-	else if (y < Y - 1){
-	  Pos_int = (1 - s) * Pos[y] + s * Pos[y+1];
-	}
-	else{
- 	  Pos_int = Pos[Y - 1];
-	}
-/*
-	Serial.print(ADCval);
-	Serial.print(' ');
-	Serial.print(x);
-	Serial.print(' ');
-	Serial.print(t);
-	Serial.print(' ');
-	Serial.print(y);
-	Serial.print(' ');
-	Serial.print(s);
-	Serial.print(' ');
-	Serial.print(Ton);
-	Serial.print(' ');
-	Serial.println(Pos_int);
-*/
-	return(Pos_int);
+  int i_Ton = Ton / 1000 - 1;
+  float t = (float)(Ton - (i_Ton + 1) * 1000) / 1000.0;
+
+  int v0 = ADCvalue[i_Ton][i_pos][i_param];
+  int v1 = ADCvalue[i_Ton + 1][i_pos][i_param];
+
+  float v = (float)v0 + t * (float)(v1 - v0);
+
+  return(v);
+}
+
+float calcVt(float Ton, float s, int i_param){
+  int k;
+  float posT = pos[0] + s * Dpos;
+  for (k = 0; k < Y - 1; k++){
+    if (pos[k] <= posT && posT < pos[k+1]) break;
+  }
+  float u = (posT - (float)pos[k]) / (float)(pos[k+1] - pos[k]);
+  float v0 = calcV(Ton, k, i_param);
+  float v1 = calcV(Ton, k+1, i_param);
+  float vt;
+  vt = v0 + u * (v1 - v0);
+  return(vt);
+}
+
+#define fabs(x) (((x)>0)?(x):(-(x)))
+
+float calcE(float vt, float Ton, float s, int i_param){
+  return(fabs(vt - calcVt(Ton, s, i_param)));
+}
+
+float calcPos(float s)
+{
+  return(pos[0] + s * Dpos);
+}
+
+//float calc_pos(int Ton, int ADCval)
+float calc_pos(float Ton, int nParam, int *param, float *vt)
+{
+  float s;
+  int i, j;
+
+  float v0, v1;
+  float t;
+  float ss, sp;
+
+  // calculate intial value of s
+  ss = 0;
+  for (i = 0; i < nParam; i++){
+    for (j = 0; j < Y - 1; j++){
+      v0 = calcV(Ton, j, param[i]);
+      v1 = calcV(Ton, j + 1, param[i]);
+      t = (vt[i] - v0) / (v1 - v0);
+      if (0 <= t && t < 1.0) break;
+    }
+    float ps = pos[j] + t * (pos[j+1] - pos[j]);
+    sp = (ps - pos[0]) / (pos[Y-1] - pos[0]);
+    //    printf("->%d %f %f %f : %f %f\n", j, t, pos[j], pos[j+1], ps, sp);
+    ss += sp;
+  }
+  s = ss / nParam;
+#define ds 0.01
+#define MU 0.0001
+#define EMAX  0.005
+  int st;
+  for (st = 0; st < 500; st++){
+    float dE = 0.0;
+    for (i = 0; i < nParam; i++){
+      //      printf(" %d %d %d %f %f\n", st, i, param[i], vt[i], calcVt(Ton, s, i));
+      dE += calcE(vt[i], Ton, s+ds, param[i]) - calcE(vt[i], Ton, s, param[i]);
+    }
+    float dss = dE / ds;
+    if (fabs(dE) < EMAX) break;
+    s = s - MU * dss;
+  }
+  return(calcPos(s));
 }
 
 #define LEN_LINE 64
 char buf[LEN_LINE];
 uint8_t pBuf = 0;
 float St = 3.0;
+uint8_t st = 0;
 
 void loop()
 {
@@ -209,13 +270,32 @@ void loop()
 		buf[pBuf++] = c;
 	}
 
+	if (digitalRead(PIN_SW) == LOW)
+	{
+		st = (st + 1) % 9;
+		Ton = 1000 + 1000 * st;
+		OCR1A = Ton * 2 - 1; // PWM Duty Cycle
+		while (digitalRead(PIN_SW) == LOW) delay(10);
+		tm = 0;
+		delay(500);
+	}
+
+
 	// set St by potentiometer
 	uint16_t St_ = analogRead(PIN_POT);
 	// 0-1023 -> St:0-4
 	St = (float)St_ * 4.0 / 1023.0;
 
 	// Position Control
-	float S = calc_pos(Ton, v1 - v0);
+
+//	float calc_pos(float Ton, int nParam, int *param, float *vt)
+	int param[2] = {0, 3}; // I(0.1), I(0.7)
+	float S = calc_pos(Ton, 2, param, (float *)vt);
+
+	Serial.print(S);
+	Serial.print(' ');
+	Serial.println(Ton);
+/*
 #define Kp 5.0
 	int16_t dTon = (uint16_t)((S - St) * Kp);
 	int16_t Ton_t = Ton + dTon;
@@ -234,6 +314,7 @@ void loop()
 	Serial.print(S);
 	Serial.print(' ');
 	Serial.println(Ton);
+*/
 }
 
 
