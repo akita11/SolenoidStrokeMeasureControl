6924efa6a5ba3c290958272d0cb988eebe4cf157
akita11 <akita@ifdl.jp>
2024-07-23 08:15:27 +0900

    Meas&Ctrl for Arduino

--- MeasCtrl_ArduinoUNO/src/main.cpp
+++ MeasCtrl_ArduinoUNO/src/main.cpp
@@ -15,7 +15,7 @@ uint16_t Tv1[] = {700, 400}; // [us]
 uint8_t iToni[] = {1, 9, 2, 8, 3, 7, 6, 4, 5};
 uint8_t fMeasure = 0;
 uint8_t fRun = 0;
-uint8_t fReady = 0;
+volatile uint8_t fReady = 0;
 
 #define BUF_LEN 64
 char buf[BUF_LEN];
@@ -37,23 +37,23 @@ void SetPWM(uint16_t Ton, uint16_t Tcycle, uint16_t Tv1s)
 ISR(TIMER1_OVF_vect)
 {
 	delayMicroseconds(100);
-	PORTD |= _BV(PD2); // D2
+//	PORTD |= _BV(PD2); // D2
 //	v0s += analogRead(PIN_ADC);
 	ADCSRA |= 0x40; // start conversion
 	while(ADCSRA & 0x40); // wait for ADC convesion
 	v0s += ADC;
-	PORTD &= ~(_BV(PD2));
+//	PORTD &= ~(_BV(PD2));
 }
 
 // Timer1 CompareMatchB interrupt
 ISR(TIMER1_COMPB_vect)
 {
-	PORTD |= _BV(PD2);
+//	PORTD |= _BV(PD2);
 //	v1s += analogRead(PIN_ADC);
 	ADCSRA |= 0x40; // start conversion
 	while(ADCSRA & 0x40); // wait for ADC convesion
 	v1s += ADC;
-	PORTD &= ~(_BV(PD2));
+//	PORTD &= ~(_BV(PD2));
 	n++;
 	if (n == Ncycle)
 	{
@@ -70,15 +70,15 @@ void setup() {
 	pinMode(PIN_LED, OUTPUT);
 	digitalWrite(PIN_LED, LOW);
 	Serial.begin(115200);
+	Serial.println("SolenoidMeasureControl v1.0");
 	// setup Timer1
 	TCCR1A = _BV(COM1A1) | _BV(WGM11);			  // Ch.A: non-inverting, WGM11=1 (mode14, Fast PWM)
 	TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11); // WGM13:12=11, CS11=1 (16MHz/8 = 2MHz)
 
 	SetPWM(Ton, Tcycle, Tv1s);
-
 	// setup ADC
 	ADMUX = 0x40; // Ref=AVCC, LeftAdj=0, MUX=0
-	ADCSRA = 0x80; // enable ADC
+	ADCSRA = 0x87; // enable ADC, ADC clock = 16MHz/128 = 125kHz / ADC clock : 50kHz - 200kHz
 
 	// enable interrupts
 	TIMSK1 |= _BV(TOIE1);  // enable Timer1 OVF interrupt (=PWM ON)
@@ -88,36 +88,12 @@ void setup() {
 
 void loop()
 {
-/*
 	if (Serial.available()){
 		char c = Serial.read();
 		if (c == '\n' || c == '\r'){
 			buf[pBuf] = '\0';
 			if (strncmp(buf, "MEASURE", 7) == 0){
-				Serial.println("Measure start");
-				fMeasure = 1;
-			}
-			else if (strncmp(buf, "PWMD", 4) == 0){
-				// set PWM duty [us]
-				Ton = atoi(buf + 4);
-				Serial.print("Ton = "; Serial.println(Ton);
-				SetPWM(Ton, Tcycle, Tv1s);
-			}
-			else{
-				if (pBuf > 0) Serial.println("?");
-			}
-			pBuf = 0;
-		}
-		else buf[pBuf++] = c;
-		if (pBuf == BUF_LEN) pBuf = 0;
-	}
-*/
-	if (Serial.available()){
-		char c = Serial.read();
-		if (c == '\n' || c == '\r'){
-			buf[pBuf] = '\0';
-			if (strncmp(buf, "MEASURE", 7) == 0){
-				Serial.println("Measure start");
+//				Serial.println("Measure start");
 				fMeasure = 1;
 			}
 			else if (strncmp(buf, "START", 5) == 0){
@@ -159,18 +135,15 @@ void loop()
 		else buf[pBuf++] = c;
 		if (pBuf == BUF_LEN) pBuf = 0;
 	}
-/*
 	#define N_SAMPLE 5
 	if (fMeasure == 1){
 		int v[9][2][2][5];
-		uint8_t iTon, iDelay;;
+		uint8_t iTon;
 		uint16_t Ton;
 		for (uint8_t ns = 0; ns < N_SAMPLE; ns++){
 			for (uint8_t f = 0; f < 2; f++){
-				M5.Display.clear(TFT_RED);
-				M5.Display.setCursor(0, 0);
-				M5.Display.printf("n=%d f=%d\n", ns, f);
 				for (iTon = 0; iTon < 9; iTon++){
+					digitalWrite(PIN_LED, 1 - digitalRead(PIN_LED));
 					Ton = iToni[iTon] * Ton0[f];
 					SetPWM(Ton, Ton0[f] * 10, Tv1[f]);
 					delay(1000);
@@ -178,37 +151,33 @@ void loop()
 					while(fReady == 0) delayMicroseconds(100);
 					v[iTon][0][f][ns] = v0;
 					v[iTon][1][f][ns] = v1;
-					M5.Display.printf("%d,%d,%d,%d\n", Ton0[f], iToni[iTon], v[iTon][0][f][ns], v[iTon][1][f][ns]);
 				}
 			}
-			SetPWM(0, Tcycle, Tv1s);
 		}
+		digitalWrite(PIN_LED, LOW);
 		for (iTon = 0; iTon < 9; iTon++){
 			for (uint8_t f = 0; f < 2; f++){
 				Ton = iToni[iTon] * Ton0[f];
-				printf("%.2f,", (float)Ton/1000);
+				Serial.print((float)Ton/1000); Serial.print(',');
 				uint32_t s0 = 0, s1 = 0;
 				for (uint8_t ns = 0; ns < N_SAMPLE; ns++){
 					s0 += v[iTon][0][f][ns];
 					s1 += v[iTon][1][f][ns];
 				}
-				printf("%.2f,%.2f,", (float)s0/(float)N_SAMPLE, (float)s1/(float)N_SAMPLE);
+				Serial.print((float)s0/(float)N_SAMPLE); Serial.print(',');
+				Serial.print((float)s1/(float)N_SAMPLE); Serial.print(',');
 			}
 			for (uint8_t f = 0; f < 2; f++){
 				for (uint8_t ns = 0; ns < N_SAMPLE; ns++){
-					printf("%d,%d,", v[iTon][0][f][ns], v[iTon][1][f][ns]);
+					Serial.print(v[iTon][0][f][ns]); Serial.print(',');
+					Serial.print(v[iTon][1][f][ns]); Serial.print(',');
 				}
 			}
-			printf("\n");
+			Serial.println("");
 		}
-		Serial2.printf("Measure done\r\n");
-		SetPWM(0, Tcycle, Tv1s);
+		SetPWM(1, Tcycle, Tv1s);
 		fMeasure = 0;
-		M5.Display.clear(TFT_BLACK);
-		M5.Display.setCursor(0, 0);
-		M5.Display.printf("Ready\n");
 	}
-*/
 	if (fRun == 1){
 		if (fReady == 1){
 			fReady = 0;
