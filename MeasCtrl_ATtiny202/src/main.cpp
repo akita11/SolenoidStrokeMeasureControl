78c3fb5219dc0a76df19b7c02b36fbb5c9acf004
akita11 <akita@ifdl.jp>
2024-08-09 14:04:34 +0900

    initial commit for ATtiny202 measure

--- MeasCtrl_ATtiny202/src/main.cpp
+++ MeasCtrl_ATtiny202/src/main.cpp
@@ -1,7 +1,16 @@
 #include <Arduino.h>
 
+// PA0 : UPDI       / D5
+// PA1 : ADC (AIN1) / D2
+// PA2 : PWM (=WO2) / D3
+// PA3 : SW         / D4
+// PA6 : TXD        / D0
+// PA7 : RXD        / D1
+
 void setup() {
+  pinMode(0, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(0, 1); delay(500); digitalWrite(0, 0); delay(500);
 }
