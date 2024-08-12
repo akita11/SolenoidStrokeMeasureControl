b6d11ce04315c059ffd06ceb5c877634d580dc1f
akita11 <akita@ifdl.jp>
2024-08-13 07:35:47 +0900

    ATtiny202 skelton

--- MeasCtrl_ATtiny202/src/main.cpp
+++ MeasCtrl_ATtiny202/src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 
-// PA0 : UPDI       / D5
-// PA1 : ADC (AIN1) / D2
-// PA2 : PWM (=WO2) / D3
-// PA3 : SW         / D4
-// PA6 : TXD        / D0
-// PA7 : RXD        / D1
+// PA0(p6) : UPDI       / D5
+// PA1(P4) : ADC (AIN1) / D2
+// PA2(p5) : PWM (=WO2) / D3
+// PA3(p7) : SW         / D4
+// PA6(p2) : TXD        / D0
+// PA7(p3) : RXD        / D1
 
 void setup() {
   pinMode(0, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(0, 1); delay(500); digitalWrite(0, 0); delay(500);
+  digitalWrite(0, 1); delay(200); digitalWrite(0, 0); delay(200);
 }
